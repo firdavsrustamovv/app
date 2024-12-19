eab79cc20c4d5d705e482d681972764729b0408c
firdavsrustamovv <rustamovfirdavs15@gmail.com>
2024-12-19 16:37:26 +0500

    finish loader

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,10 +1,12 @@
 import { useSelector } from "react-redux";
+import Loader from "../ui/loader";
 
 function Main() {
-  const { articles } = useSelector((state) => state.article);
+  const { articles, isLoading } = useSelector((state) => state.article);
   return (
     <div classNameName="container">
-      <div classNameName="album py-5">
+      {isLoading && <Loader />}
+      <div className="album py-5">
         <div className="container">
           <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
             {articles.map((iteam) => (
@@ -24,33 +26,33 @@ function Main() {
                     <rect width="100%" height="100%" fill="#55595c"></rect>
                   </svg>
                   <div className="card-body">
-                    <p className="card-text fw-bold">{iteam.title}</p>
+                    <p className="card-text fw-bold m-0">{iteam.title}</p>
                     <p className="card-text">{iteam.description}</p>
-                    <div className="d-flex justify-content-between align-items-center">
-                      <div className="btn-group">
-                        <button
-                          type="button"
-                          className="btn btn-sm btn-outline-success"
-                        >
-                          View
-                        </button>
-                        <button
-                          type="button"
-                          className="btn btn-sm btn-outline-secondary"
-                        >
-                          Edit
-                        </button>
-                        <button
-                          type="button"
-                          className="btn btn-sm btn-outline-danger"
-                        >
-                          Delete
-                        </button>
-                      </div>
-                      <small className="text-body-secondary fw-bold text-capitalize">
-                        {iteam.author.username}
-                      </small>
+                  </div>
+                  <div className="card-footer d-flex justify-content-between align-items-center">
+                    <div className="btn-group">
+                      <button
+                        type="button"
+                        className="btn btn-sm btn-outline-success"
+                      >
+                        View
+                      </button>
+                      <button
+                        type="button"
+                        className="btn btn-sm btn-outline-secondary"
+                      >
+                        Edit
+                      </button>
+                      <button
+                        type="button"
+                        className="btn btn-sm btn-outline-danger"
+                      >
+                        Delete
+                      </button>
                     </div>
+                    <small className="text-body-secondary fw-bold text-capitalize">
+                      {iteam.author.username}
+                    </small>
                   </div>
                 </div>
               </div>
---
+++ src/ui/loader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Loader() {
+  return (
+    <div class="spinner-border d-block mx-auto" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  );
+}
+
+export default Loader;
