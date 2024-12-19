d19033cab5a5b44f8ae5ef78894c803b5c02a929
firdavsrustamovv <rustamovfirdavs15@gmail.com>
2024-12-19 15:50:13 +0500

    start main page

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,5 +1,65 @@
+import { useSelector } from "react-redux";
+
 function Main() {
-  return <div>Main</div>;
+  const { articles } = useSelector((state) => state.article);
+  return (
+    <div classNameName="container">
+      <div classNameName="album py-5">
+        <div className="container">
+          <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            {articles.map((iteam) => (
+              <div className="col" key={iteam.id}>
+                <div className="card h-100 shadow-sm">
+                  <svg
+                    className="bd-placeholder-img card-img-top"
+                    width="100%"
+                    height="225"
+                    xmlns="http://www.w3.org/2000/svg"
+                    role="img"
+                    aria-label="Placeholder: Thumbnail"
+                    preserveAspectRatio="xMidYMid slice"
+                    focusable="false"
+                  >
+                    <title>Placeholder</title>
+                    <rect width="100%" height="100%" fill="#55595c"></rect>
+                  </svg>
+                  <div className="card-body">
+                    <p className="card-text fw-bold">{iteam.title}</p>
+                    <p className="card-text">{iteam.description}</p>
+                    <div className="d-flex justify-content-between align-items-center">
+                      <div className="btn-group">
+                        <button
+                          type="button"
+                          className="btn btn-sm btn-outline-success"
+                        >
+                          View
+                        </button>
+                        <button
+                          type="button"
+                          className="btn btn-sm btn-outline-secondary"
+                        >
+                          Edit
+                        </button>
+                        <button
+                          type="button"
+                          className="btn btn-sm btn-outline-danger"
+                        >
+                          Delete
+                        </button>
+                      </div>
+                      <small className="text-body-secondary fw-bold text-capitalize">
+                        {iteam.author.username}
+                      </small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Main;
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import AuthSlice from "../slice/auth";
+import ArticleSlice from "../slice/article";
 
 export default configureStore({
   reducer: {
     auth: AuthSlice,
+    article: ArticleSlice,
   },
   devTools: process.env.NODE_ENV !== "production",
 });
